ab0095f967d32323a64e1d4eaf7a140066fd2f6a
Josh Soref <jsoref@gmail.com>
2022-01-09 01:27:57 -0500

    spelling: object
    Signed-off-by: Josh Soref <jsoref@gmail.com>

--- hw/usb/dev-mtp.c
+++ hw/usb/dev-mtp.c
@@ -1121,7 +1121,7 @@ static MTPData *usb_mtp_get_object_prop_value(MTPState *s, MTPControl *c,
         break;
     case PROP_PERSISTENT_UNIQUE_OBJECT_IDENTIFIER:
         /* Should be persistent between sessions,
-         * but using our objedt ID is "good enough"
+         * but using our object ID is "good enough"
          * for now */
         usb_mtp_add_u64(d, 0x0000000000000000);
         usb_mtp_add_u64(d, o->handle);
