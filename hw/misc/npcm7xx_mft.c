a5bd36beaa07a87bd054313c7854dcedd8e55dd9
Josh Soref <jsoref@gmail.com>
2022-01-09 01:27:01 -0500

    spelling: conditions
    Signed-off-by: Josh Soref <jsoref@gmail.com>

--- hw/misc/npcm7xx_mft.c
+++ hw/misc/npcm7xx_mft.c
@@ -205,7 +205,7 @@ static NPCM7xxMFTCaptureState npcm7xx_mft_compute_cnt(
 
 /*
  * Capture Fan RPM and update CNT and CR registers accordingly.
- * Raise IRQ if certain contidions are met in IEN.
+ * Raise IRQ if certain conditions are met in IEN.
  */
 static void npcm7xx_mft_capture(NPCM7xxMFTState *s)
 {
